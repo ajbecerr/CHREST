f7fe26d784c7bd80a2e71917987a69588bca7856
Alejandro Becerra <ajbecerr@srv-p22-13.cbls.ccr.buffalo.edu>
2022-05-17 14:54:54 -0400

    update slurm, .err, and .out files

--- testing/DAKOTAtest/driver_fail/slurm.sh
+++ testing/DAKOTAtest/driver_fail/slurm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=debug
-#SBATCH --qos=debug
+#SBATCH --partition=general-compute
+#SBATCH --qos=general-compute
 #SBATCH --job-name='chem_sens'
 #SBATCH --output=out_chem_sens-%j.out
 #SBATCH --error=error_chem_sens-%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=01:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mail-user=ajbecerr@buffalo.edu
 #SBATCH --mail-type=ALL
 module use /projects/academic/chrest/modules
@@ -17,5 +17,6 @@ export DAK_INSTALL=/util/academic/dakota/dakota-6.15.0-release-public-rhel7.x86_
 export PATH=$DAK_INSTALL/bin:$DAK_INSTALL/share/dakota/test:$PATH
 export PYTHONPATH=$DAK_INSTALL/share/dakota/Python:$PYTHONPATH
 source /util/academic/intel/20.2/compilers_and_libraries_2020.2.254/linux/mpi/intel64/bin/mpivars.sh
+source $MKL/bin/mklvars.sh
 srun dakota -i sensitivity.in
 
