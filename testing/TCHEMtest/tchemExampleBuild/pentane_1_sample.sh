60830b68a5d12a6d9c8cc6f14a53424cf12036dc
Alejandro Becerra <ajbecerr@srv-p22-13.cbls.ccr.buffalo.edu>
2022-05-26 17:16:53 -0400

    update TCHEM tests

--- testing/TCHEMtest/tchemExampleBuild/pentane_1_sample.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-module use /projects/academic/chrest/modules
-module load chrest/release
-./exe --useYaml=true --chemfile=../tchemExample/inputs/LL2KGB_AllRange_UnitsTesting.yaml --samplefile=../tchemExample/inputs/sample1.dat
