71ebb8a32bb54d6bf8cde24c286f1a4fdb24f4c0
Alex Becerra <abecer02@login-prod-01.pax.tufts.edu>
2022-06-27 14:43:03 -0400

    patralab ABLATE tests

--- testing/ABLATEtest/2S_CH4_CM2/slurm.sh
+++ testing/ABLATEtest/2S_CH4_CM2/slurm.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #SBATCH -J ABLATEtest_2S_CH4_CM2
-#SBATCH -p mpi #run on mpi queue
-#SBATCH --time=0-1:00:00 #day-hour:minute:second
+#SBATCH -p batch #run on batch queue
 #SBATCH -N 1 #request 1 cores
 #SBATCH --mem=2000 #request 2000MG memory
-#SBATCH --output=%j.out #your output file
-#SBATCH --error=%j.err #your error file
+#SBATCH --output=outfile #your output file
+#SBATCH --error=errfile #your error file
 #SBATCH	--mail-type=ALL
 #SBATCH	--mail-user=ajbecerr@buffalo.edu
-bash 2S_CH4_CM2.sh
+srun 2S_CH4_CM2.sh
