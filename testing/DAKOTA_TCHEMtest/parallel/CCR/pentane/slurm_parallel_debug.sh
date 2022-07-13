fa50724f7d20aec9d935a0015307711e799a7408
Alejandro Becerra <ajbecerr@srv-p22-12.cbls.ccr.buffalo.edu>
2022-07-13 11:27:31 -0400

    parallel testing

--- testing/DAKOTA_TCHEMtest/parallel/CCR/pentane/slurm_parallel_debug.sh
+++ testing/DAKOTA_TCHEMtest/parallel/CCR/pentane/slurm_parallel_debug.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH --constraint=CPU-Gold-6130
-#SBATCH --partition=general-compute
-#SBATCH --qos=general-compute
+#SBATCH --partition=debug
+#SBATCH --qos=debug
 #SBATCH --job-name='chem_sens'
 #SBATCH --output=out_chem_sens_parallel-%j.out
 #SBATCH --error=error_chem_sens_parallel-%j.err
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
-#SBATCH --time=72:00:00
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=01:00:00
 #SBATCH --mail-user=ajbecerr@buffalo.edu
 #SBATCH --mail-type=ALL
 module purge
